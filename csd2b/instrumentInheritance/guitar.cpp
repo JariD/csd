74c84b03fd936d4709e5aba718dbb1fd2135816a
J <JariDeelstra@gmail.com>
2023-11-27 18:53:23 +0100

    updated assignment

---
+++ csd2b/instrumentInheritance/guitar.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+#include "guitar.h"
+
+guitar::guitar()
+{
+
+
+}
---
+++ csd2b/instrumentInheritance/main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include "instrument.h"
+#include "guitar.h"
+#include "violin.h"
+
+int main()
+{
+
+
+    return 0;
+}
