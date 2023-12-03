2c53c6272fa335b98a241a7fe095eda9b4e00a9a
J <JariDeelstra@gmail.com>
2023-12-03 18:18:46 +0100

    Added reflection.md

---
+++ csd2b/synth/reflection.md
@@ -0,0 +1,15 @@
+# Reflection assignment
+
+## Approach plan
+
+## Process
+### What went well:
+
+### What could've gone better?
+
+### What did I learn
+
+## End result
+
+## Take aways
+### What 
---
+++ csd2b/synth/sine.h
@@ -0,0 +1,14 @@
+#ifndef _SINE_H_
+#define _SINE_H_
+#include <iostream>
+#include <cmath>
+#include "oscillator.h"
+
+class Sine : public Oscillator
+{
+public:
+    Sine();
+    ~Sine();
+};
+
+#endif
