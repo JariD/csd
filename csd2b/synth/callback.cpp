6dd174a0e8dee1c57b1cc2f9912415f79c787c71
J <JariDeelstra@gmail.com>
2023-12-15 00:09:15 +0100

    updated macro

--- csd2b/synth/callback.cpp
+++ csd2b/synth/callback.cpp
@@ -6,7 +6,9 @@
 #include "square.h"
 
 //parts of the code are from csd github
+#define singleOsc 0
 
+#if singleOsc
 void CustomCallback::process(AudioBuffer buffer) {
   for (int i = 0; i < buffer.numFrames; ++i) {
     // write sample to buffer at channel 0, amp = 0.25
@@ -21,46 +23,46 @@ void CustomCallback::prepare(int rate) {
     std::cout << "\nsamplerate: " << sampleRate << "\n";
 }
 
+#else
+double mtof(float mPitch)
+{
+    return 440.0 * pow(2.0, (mPitch - 69.0f)/12.0f);
+} // mtof()
 
-//double mtof(float mPitch)
-//{
-//    return 440.0 * pow(2.0, (mPitch - 69.0f)/12.0f);
-//} // mtof()
-//
-//void updatePitch(Melody& melody, Triangle& triangle) {
-//    float note = melody.getNote();
-//    double freq = mtof(note);
-//    std::cout << "next note: " << note << ", has frequency " << freq << std::endl;
-//    triangle.setFrequency(freq);
-//} // updatePitch()
-//
-//void CustomCallback::prepare (int rate) {
-//    this->sampleRate=rate;
-//    updatePitch(melody,triangle);
-//} // new prepare()
-//
-//void CustomCallback::process (AudioBuffer buffer) {
-//    auto [inputChannels, outputChannels, numInputChannels, numOutputChannels, numFrames] = buffer;
-//
-//    for (int channel = 0; channel < numOutputChannels; ++channel) {
-//        for (int sample = 0; sample < numFrames; ++sample) {
-//            outputChannels[channel][sample] = triangle.getSample() * amplitude;
-//            triangle.tick(); // rather mixed up functionality
-//
-///* After every sample, check if we need to advance to the next note
-// * This is a bit awkward in this scheme of buffers per channel
-// *  In a multichannel setting we should update pitches independently per channel!
-// */
-//            if(frameIndex >= noteDelayFactor * sampleRate) {
-//// reset frameIndex
-//                frameIndex = 0;
-//                updatePitch(melody,triangle);
-//            }
-//            else {
-//// increment frameindex
-//                frameIndex++;
-//            }
-//        } // for sample
-//    } // for channel
-//}
-//
+void updatePitch(Melody& melody, Triangle& triangle) {
+    float note = melody.getNote();
+    double freq = mtof(note);
+    std::cout << "next note: " << note << ", has frequency " << freq << std::endl;
+    triangle.setFrequency(freq);
+} // updatePitch()
+
+void CustomCallback::prepare (int rate) {
+    this->sampleRate=rate;
+    updatePitch(melody,triangle);
+} // new prepare()
+
+void CustomCallback::process (AudioBuffer buffer) {
+    auto [inputChannels, outputChannels, numInputChannels, numOutputChannels, numFrames] = buffer;
+
+    for (int channel = 0; channel < numOutputChannels; ++channel) {
+        for (int sample = 0; sample < numFrames; ++sample) {
+            outputChannels[channel][sample] = triangle.getSample() * amplitude;
+            triangle.tick(); // rather mixed up functionality
+
+/* After every sample, check if we need to advance to the next note
+ * This is a bit awkward in this scheme of buffers per channel
+ *  In a multichannel setting we should update pitches independently per channel!
+ */
+            if(frameIndex >= noteDelayFactor * sampleRate) {
+// reset frameIndex
+                frameIndex = 0;
+                updatePitch(melody,triangle);
+            }
+            else {
+// increment frameindex
+                frameIndex++;
+            }
+        } // for sample
+    } // for channel
+}
+#endif
--- csd2b/synth/fm.cpp
+++ csd2b/synth/fm.cpp
@@ -1,3 +1,7 @@
 #include "fm.h"
 #include <iostream>
 
+void FM::freqModulation(float modFreq)
+{
+    //calculate function for the oscillators
+}
