b605ae1c316ca34d58ffcb1d91e855dccaf79416
J <JariDeelstra@gmail.com>
2024-02-12 13:54:08 +0100

    updated some files

---
+++ csd2c/dsp/lowpass.cpp
@@ -0,0 +1,17 @@
+#include math.h
+
+// Y[n]=0.5x[n]+0.5x[n]
+/*
+ *float filter (float x)
+ * {
+ * float y:
+ * static float xprev:
+ * return y;
+ * }
+ */
+
+x =
+// SINUS
+sample = sin(M_PI * 2 * phase) * amplitude;
+
+int main()
